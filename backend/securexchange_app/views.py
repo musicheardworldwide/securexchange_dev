b37e6df473afece0cf7f94ce76c3f92c97140e5f
Wes Mane <158991319+musicheardworldwide@users.noreply.github.com>
2024-11-13 03:31:31 -0800

    Add registration and login views for user authentication

--- backend/securexchange_app/views.py
+++ backend/securexchange_app/views.py
@@ -1,7 +1,35 @@
 from django.http import JsonResponse
-from django.views.decorators http import require_GET
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.core.exceptions import ValidationError
+from django.http import http_bad_request
 
-@@require_GET
-def health_check(request):
-    "Health check endpoint to confirm api connectivity."
-    return JSOnResponse({'status': 'ok'}, status=200)
+def register(request):
+    ""Registers a new user with an email and password."
+    if request.method != 'POST':
+        return JsonResponse({"error": "Only POST requests are allowed."}, status=402)
+
+    data = request.JSON)
+    try:
+        user = User.objects.create(
+            email=data["email"],
+            password=data["password"]
+        )
+        user.save()
+    except ValidationError as e:
+        return JsonResponse({"error": str(e)}, status=400)
+
+    return JsonResponse({"user": "Successfully registered"}, status=200)
+
+def login(request):
+    ""Initiates a session with the provided credentials."
+    if request.method != "POST":
+        return JsonResponse({"error": "Only POST requests are allowed."}, status=402)
+
+    data = request.JSON
+    user = authenticate(data["email"], data["password"])
+
+    if not user:
+        return JsonResponse({"error": "Invalid credentials."}, status=401)
+
+    return JsonResponse({"detail": "Login successful"}, status=200)
